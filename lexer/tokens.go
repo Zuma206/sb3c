78983fb97f01df44239a7125dfe01f69317308f3
James <zuma@zuma.eu.org>
2025-12-28 03:07:54 +0000

    Documented lexer token structs

--- lexer/tokens.go
+++ lexer/tokens.go
@@ -1,15 +1,27 @@
 package lexer
 
+// Marks a position in a file
 type Position struct {
-	Index, LineNumber, LineOffset int
+	// The byte index into the file
+	Index int
+	// The line number, starting at 1
+	LineNumber int
+	// The byte index into the line, starting at 1
+	LineOffset int
 }
 
+// Represents a section of source code in a file
 type Section struct {
+	// The start positon of the section
 	Pos Position
+	// The source code of the section
 	Src []byte
 }
 
+// Represents a lex token
 type Token struct {
+	// The type of lex token it was parsed as
 	Type *Type
+	// The section of code this token is
 	Section
 }
