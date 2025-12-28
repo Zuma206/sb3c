8a9107da433e326cf1fc914b011b196a8d5ceb14
James <zuma@zuma.eu.org>
2025-12-28 17:30:36 +0000

    Added token type set constructor

--- lexer/types.go
+++ lexer/types.go
@@ -1,6 +1,9 @@
 package lexer
 
-import "regexp"
+import (
+	"regexp"
+	"strings"
+)
 
 // Represents a type of token a lexer can encounter
 type Type struct {
@@ -15,4 +18,18 @@ func NewType(name string, regex string) *Type {
 		Name:  name,
 		regex: regexp.MustCompile("^" + regex),
 	}
+}
+
+// Creates a lex token type that matches to any one string from a set
+func NewTypeSet(name string, set []string) *Type {
+	var regex strings.Builder
+	regex.WriteRune('(')
+	for i, str := range set {
+		if i > 0 {
+			regex.WriteRune('|')
+		}
+		regex.WriteString(regexp.QuoteMeta(str))
+	}
+	regex.WriteRune(')')
+	return NewType(name, regex.String())
 }
