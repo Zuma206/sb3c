29ec157df951f6a9a119bdb948f9d3e2952c2511
James <zuma@zuma.eu.org>
2025-12-28 17:37:13 +0000

    Added basic class definition tokens

---
+++ language/tokens.go
@@ -0,0 +1,26 @@
+package language
+
+import "github.com/zuma206/sb3c/lexer"
+
+// String literal keywords & symbols
+const (
+	OpenScope  = "{"
+	CloseScope = "}"
+	Class      = "class"
+	Extends    = "extends"
+)
+
+// Token type sets
+var (
+	Symbols  = []string{OpenScope, CloseScope}
+	Keywords = []string{Class, Extends}
+)
+
+// Token types
+var (
+	Identifier = lexer.NewType("Identifier", `[A-z$_][0-z$_]*`)
+	Keyword    = lexer.NewTypeSet("Keyword", Keywords)
+	Symbol     = lexer.NewTypeSet("Symbol", Symbols)
+)
+
+var Types = []*lexer.Type{Symbol, Keyword, Identifier}
