c779b666e592ec7db1b5466124bc9cba5e8a164b
James <zuma@zuma.eu.org>
2025-12-28 15:04:42 +0000

    Added types and next to lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,6 +1,6 @@
 package lexer
 
-import "github.com/zuma206/sb3c/utils"
+import "errors"
 
 // Lexes a file into a list of tokens and errors
 type Lexer struct {
@@ -8,21 +8,27 @@ type Lexer struct {
 	errors []*Section
 	// Lex tokens parsed so far
 	tokens []*Token
+	// The types of tokens the lexer can parse
+	types []*Type
 	// The current working position of the lexer in the file
 	pos Position
 	// The source code being lexed
 	src []byte
+	// The index of the next token to be processed
+	next int
 }
 
-func NewLexer(src []byte) *Lexer {
+func NewLexer(src []byte, types []*Type) *Lexer {
 	return &Lexer{
 		errors: make([]*Section, 0),
 		tokens: make([]*Token, 0),
+		types:  types,
 		pos: Position{
 			LineNumber: 1,
 			LineOffset: 1,
 			Index:      0,
 		},
-		src: src,
+		src:  src,
+		next: 0,
 	}
 }
