d22816c427870e3fecb8f0b4856caa8771bad173
James <zuma@zuma.eu.org>
2026-01-02 17:49:52 +0000

    Added matcher for token's type

--- parser/matching.go
+++ parser/matching.go
@@ -4,3 +4,10 @@ import "github.com/zuma206/sb3c/lexer"
 
 // A matcher takes in a token and performs validation/matching on it
 type Matcher func(token *lexer.Token) bool
+
+// Creates a matcher for a token's type
+func Type(tokenType *lexer.Type) Matcher {
+	return func(token *lexer.Token) bool {
+		return token.Type == tokenType
+	}
+}
