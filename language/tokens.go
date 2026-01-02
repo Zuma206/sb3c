79287327f2d3122cf7acbdb5328e0c59ecb9d501
James <zuma@zuma.eu.org>
2026-01-02 00:01:23 +0000

    Added whitespace token set

--- language/tokens.go
+++ language/tokens.go
@@ -12,15 +12,17 @@ const (
 
 // Token type sets
 var (
-	Symbols  = []string{OpenScope, CloseScope}
-	Keywords = []string{Class, Extends}
+	Whitespaces = []string{" ", "\n", "\r", "\t"}
+	Symbols     = []string{OpenScope, CloseScope}
+	Keywords    = []string{Class, Extends}
 )
 
 // Token types
 var (
 	Identifier = lexer.NewType("Identifier", `[A-z$_][0-z$_]*`)
+	Whitespace = lexer.NewTypeSet("Whitespace", Whitespaces)
 	Keyword    = lexer.NewTypeSet("Keyword", Keywords)
 	Symbol     = lexer.NewTypeSet("Symbol", Symbols)
 )
 
-var Types = []*lexer.Type{Symbol, Keyword, Identifier}
+var Types = []*lexer.Type{Symbol, Keyword, Identifier, Whitespace}
