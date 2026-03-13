56c5cc4124b6c9e6bbe31f6e0642852d17aae9f9
James <zuma@zuma.eu.org>
2026-03-13 03:26:53 +0000

    Added class declaration parsing

--- language/parsers.go
+++ language/parsers.go
@@ -8,5 +8,30 @@ import (
 // Parses a program (AST root)
 func ParseProgram(p *parser.Parser) (*Program, error) {
 	program := &Program{Declarations: utils.NewList[*ClassDeclaration]()}
+	classDeclaration, err := parseClassDeclaration(p)
+	if err != nil {
+		return nil, err
+	}
+	program.Declarations.PushBack(classDeclaration)
 	return program, nil
+}
+
+func parseClassDeclaration(p *parser.Parser) (*ClassDeclaration, error) {
+	classDeclaration := &ClassDeclaration{Declarations: utils.NewList[*MethodDeclaration]()}
+	if err := p.Parse([]*parser.ParseStep{
+		{Matcher: Keyword.WithSource(Class)},
+		{Matcher: Whitespace},
+		{Matcher: Identifier, Result: &classDeclaration.Name},
+		{Matcher: Whitespace},
+		{Matcher: Keyword.WithSource(Extends)},
+		{Matcher: Whitespace},
+		{Matcher: Identifier, Result: &classDeclaration.Super},
+		{Matcher: Whitespace, Optional: true},
+		{Matcher: Symbol.WithSource(OpenBrace)},
+		{Matcher: Whitespace, Optional: true},
+		{Matcher: Symbol.WithSource(CloseBrace)},
+	}); err != nil {
+		return nil, err
+	}
+	return classDeclaration, nil
 }
