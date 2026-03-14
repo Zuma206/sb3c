3f934ae4a2d6d104f716081d38391be35c9494cf
James <zuma@zuma.eu.org>
2026-03-14 16:25:24 +0000

    Added sb3 artifact to compile procedure

--- compiler/compile.go
+++ compiler/compile.go
@@ -1,9 +1,12 @@
 package compiler
 
 import (
+	"os"
+
 	"github.com/zuma206/sb3c/language"
 	"github.com/zuma206/sb3c/lexer"
 	"github.com/zuma206/sb3c/parser"
+	"github.com/zuma206/sb3c/sb3"
 	"github.com/zuma206/sb3c/visualisation"
 )
 
@@ -17,5 +20,12 @@ func CompileFile(name string, src []byte) error {
 		return err
 	}
 	visualisation.Visualise(program)
+	outfile, err := os.OpenFile("project.sb3", os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	if _, err := sb3.NewSB3().WriteTo(outfile); err != nil {
+		return err
+	}
 	return nil
 }
