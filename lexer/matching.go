a75321f239a79d9bbad25c3923b10538620cd0ff
James <zuma@zuma.eu.org>
2026-04-02 00:42:20 +0100

    Added lexer.MatchAny to match any matcher in a set

--- lexer/matching.go
+++ lexer/matching.go
@@ -1,5 +1,10 @@
 package lexer
 
+import (
+	"errors"
+	"fmt"
+)
+
 // A matcher takes in a token and performs validation/matching on it
 type MatcherFunc func(token *Token) error
 
@@ -12,4 +17,18 @@ func (matcher MatcherFunc) MatchLexToken(token *Token) error {
 type Matcher interface {
 	// Returns an error if the token does not match the criteria
 	MatchLexToken(*Token) error
+}
+
+var MatchAnyErr = errors.New("match any error")
+
+func MatchAny(matchers ...Matcher) Matcher {
+	return MatcherFunc(func(token *Token) error {
+		var err error
+		for _, matcher := range matchers {
+			if err = matcher.MatchLexToken(token); err != nil {
+				return nil
+			}
+		}
+		return fmt.Errorf("%w: %w", MatchAnyErr, err)
+	})
 }
