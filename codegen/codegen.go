0ede60c516707e08b81f969a36d762dab4b2bd4c
James <zuma@zuma.eu.org>
2026-04-03 00:12:11 +0100

    Added variable codegen

--- codegen/codegen.go
+++ codegen/codegen.go
@@ -4,54 +4,62 @@ import (
 	"errors"
 	"fmt"
 	"iter"
+	"strconv"
+	"strings"
 
 	"github.com/zuma206/sb3c/language"
 	"github.com/zuma206/sb3c/sb3"
 )
 
-type CodeGenerator struct {
-	sb3 *sb3.SB3
-}
-
 func Generate(program *language.Program) (*sb3.SB3, error) {
 	sb3Project := sb3.NewSB3()
-	generator := &CodeGenerator{sb3: sb3Project}
-	err := generator.generate(program)
-	return sb3Project, err
+	for class := range program.Classes.Iter() {
+		target, err := newTarget(sb3Project, class)
+		if err != nil {
+			return nil, err
+		}
+		if err := generateMembers(target, class); err != nil {
+			return nil, err
+		}
+	}
+	return sb3Project, nil
 }
-
-var InvalidSuperError = errors.New("invalid super")
 
 const (
 	StageClass  = "Stage"
 	SpriteClass = "Sprite"
 )
 
-func (cg *CodeGenerator) generate(program *language.Program) error {
-	for declaration := range program.Classes.Iter() {
-		switch declaration.Super.Src {
-		case StageClass:
-			if err := cg.generateStage(declaration); err != nil {
-				return err
-			}
-		default:
-			return fmt.Errorf("%w: %q", InvalidSuperError, declaration.Super.Src)
+var InvalidSuperError = errors.New("invalid super")
+
+func newTarget(sb3Project *sb3.SB3, class *language.Class) (*sb3.TargetHnd, error) {
+	switch class.Super.Src {
+	case StageClass:
+		return sb3Project.NewStage()
+	default:
+		err := fmt.Errorf("%q is an invalid super class %w", class.Name.Src, class.Name)
+		return nil, errors.Join(InvalidSuperError, err)
+	}
+}
+
+func generateMembers(target *sb3.TargetHnd, class *language.Class) error {
+	for member := range class.Members.Iter() {
+		if err := generateMember(target, member); err != nil {
+			return err
 		}
 	}
 	return nil
 }
 
-func (cg *CodeGenerator) generateStage(class *language.Class) error {
-	stage, err := cg.sb3.NewStage()
-	if err != nil {
-		return fmt.Errorf("%w %w", err, &class.Super.Pos)
+func generateMember(target *sb3.TargetHnd, member *language.Member) error {
+	switch {
+	case member.Value.Method != nil:
+		return generateProcedure(target, member)
+	case member.Value.Attribute != nil:
+		return generateVariable(target, member)
+	default:
+		panic("malformed class member")
 	}
-	for method := range class.Members.Iter() {
-		if err := cg.generateProcedure(stage, method); err != nil {
-			return err
-		}
-	}
-	return nil
 }
 
 var (
@@ -59,7 +67,7 @@ var (
 	MissingArgumentErr = errors.New("missing argument")
 )
 
-func (cg *CodeGenerator) generateProcedure(target *sb3.TargetHnd, method *language.Member) error {
+func generateProcedure(target *sb3.TargetHnd, method *language.Member) error {
 	procedure := target.NewProcedure(method.Name.Src)
 	for call := range method.Value.Method.Calls.Iter() {
 		mapping, ok := mappings[call.Path.Src]
@@ -79,4 +87,27 @@ func (cg *CodeGenerator) generateProcedure(target *sb3.TargetHnd, method *langua
 		procedure.PushBlock(block)
 	}
 	return nil
+}
+
+func generateVariable(target *sb3.TargetHnd, attribute *language.Member) error {
+	initialValue, err := generateVariableInitialValue(attribute.Value.Attribute)
+	if err != nil {
+		return err
+	}
+	target.NewVariable(attribute.Name.Src, initialValue)
+	return nil
+}
+
+var InvalidInitalValueErr = errors.New("invalid attribute initial value")
+
+func generateVariableInitialValue(attribute *language.Attribute) (any, error) {
+	if attribute.Initializer == nil {
+		return "", nil
+	}
+	switch attribute.Initializer.Type {
+	case language.NumberLiteral:
+		return strconv.Atoi(strings.ReplaceAll(attribute.Initializer.Src, "_", ""))
+	default:
+		return nil, errors.Join(InvalidInitalValueErr, &attribute.Initializer.Pos)
+	}
 }
