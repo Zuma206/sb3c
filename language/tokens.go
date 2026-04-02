c84130ab5781f6803b24e3738d99e182bb978b19
James <zuma@zuma.eu.org>
2026-04-02 21:11:25 +0100

    Fixed token regexes matching incorrect symbols between 9 and A, A and a, etc

--- language/tokens.go
+++ language/tokens.go
@@ -1,6 +1,8 @@
 package language
 
-import "github.com/zuma206/sb3c/lexer"
+import (
+	"github.com/zuma206/sb3c/lexer"
+)
 
 // String literal keywords & symbols
 const (
@@ -23,11 +25,17 @@ var (
 	Keywords = []string{ClassKeyword, Extends}
 )
 
+// Regexes
+var (
+	identifierRegex = `[A-Za-z$_][0-9a-zA-Z$_]*`
+	pathRegex       = identifierRegex + `(\.` + identifierRegex + `)*`
+)
+
 // Token types
 var (
 	NumberLiteral = lexer.NewType("NumberLiteral", `[0-9]([0-9_]*[0-9])?`)
-	Identifier    = lexer.NewType("Identifier", `[A-z$_][0-z$_]*`)
-	Path          = lexer.NewType("Path", `[A-z$_](\.?[0-z$_])*`)
+	Identifier    = lexer.NewType("Identifier", identifierRegex)
+	Path          = lexer.NewType("Path", pathRegex)
 	Whitespace    = lexer.NewType("Whitespace", `[\n\r\t\ ]+`)
 	Keyword       = lexer.NewTypeSet("Keyword", Keywords)
 	Symbol        = lexer.NewTypeSet("Symbol", Symbols)
