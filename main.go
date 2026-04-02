bff0e78109a85132350c8952c95dc3a9845a02ab
James <zuma@zuma.eu.org>
2026-04-02 22:13:47 +0100

    Changed error printing to happen in red

--- main.go
+++ main.go
@@ -7,10 +7,15 @@ import (
 	"github.com/zuma206/sb3c/compiler"
 )
 
+const (
+	colorRed  = "\033[0;31m"
+	colorNone = "\033[0m"
+)
+
 // Main compiled binary entry point, calls the compiler and prints fatal errors to stderr
 func main() {
 	if err := compiler.Main(); err != nil {
-		fmt.Fprintf(os.Stderr, "fatal error: %s\n", err.Error())
+		fmt.Fprintln(os.Stderr, colorRed+"fatal error:", err.Error(), colorNone)
 		os.Exit(1)
 	}
 }
