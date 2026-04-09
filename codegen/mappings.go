b83978cb90c6f236271a2a72f57df6b5918b07b6
James <zuma@zuma.eu.org>
2026-04-09 18:51:00 +0100

    Added method decorator mappings

--- codegen/mappings.go
+++ codegen/mappings.go
@@ -1,5 +1,7 @@
 package codegen
 
+import "github.com/zuma206/sb3c/sb3"
+
 type BlockMapping struct {
 	Opcode string
 	Inputs []string
@@ -7,4 +9,23 @@ type BlockMapping struct {
 
 var mappings = map[string]*BlockMapping{
 	"this.motion.moveSteps": {Opcode: "motion_movesteps", Inputs: []string{"STEPS"}},
+}
+
+type ProcedureDecoratorMapping func(procedure *sb3.ProcedureHnd)
+
+var procedureDecoratorMappings = map[string]ProcedureDecoratorMapping{
+	"whenGreenFlagClicked": procedureCallDecorator("event_whenflagclicked"),
+}
+
+func procedureCallDecorator(opcode string) ProcedureDecoratorMapping {
+	return func(procedure *sb3.ProcedureHnd) {
+		blockThread := procedure.Target().NewBlockThread()
+		blockThread.PushBlock(&sb3.Block{
+			Opcode:   opcode,
+			Inputs:   map[string]*sb3.Input{},
+			Fields:   struct{}{},
+			TopLevel: true,
+		})
+		blockThread.PushBlock(procedure.Call())
+	}
 }
