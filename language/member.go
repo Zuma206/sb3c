4301d2f174731e8285245954d600f3dbd019bc50
Zuma <zuma@zuma.eu.org>
2026-09-24 20:29:21 +0100

    Migrated parsing attriubteOrMethod

--- language/member.go
+++ language/member.go
@@ -11,7 +11,7 @@ import (
 type Member struct {
 	Decorators        *utils.List[*Call]
 	Name              *lexer.Token
-	AttributeOrMethod AttributeOrMethod
+	AttributeOrMethod *AttributeOrMethod
 }
 
 type AttributeOrMethod struct {
@@ -29,34 +29,17 @@ var member = parser.Value(func(member *Member) parser.Parse {
 		parser.Optional(parser.Type(Whitespace)),
 		parser.Store(&member.Name, parser.Type(Identifier)),
 		parser.Optional(parser.Type(Whitespace)),
-		parser.Store(&member.AttributeOrMethod, parser.Func(parseAttributeOrMethod)),
+		parser.Store(&member.AttributeOrMethod, attributeOrMethod),
 	)
 })
 
-var MemberSymbolErr = errors.New("invalid member symbol")
-
-func parseAttributeOrMethod(p *parser.Parser) (AttributeOrMethod, error) {
-	symbol, err := p.ConsumeIf(lexer.MatchAny(
-		Symbol.WithSource(Equals), Symbol.WithSource(Semicolon), Symbol.WithSource(OpenBracket)))
-	value := MemberValue{}
-	if err != nil {
-		return value, errors.Join(MemberSymbolErr, err)
-	}
-	switch symbol.Src {
-	case Equals:
-		value.Attribute, err = parseAttribute(p)
-	case Semicolon:
-		value.Attribute = &Attribute{}
-	case OpenBracket:
-		value.Method, err = parseMethod(p)
-	default:
-		// If this panic triggers, check the switch statement has a case for every lexer.MatchAny param
-		panic("class member parsed invalid symbol as correct")
-	}
-	return value, err
-}
-
-var AttributeErr = errors.New("failed to parse attribute")
+var attributeOrMethod = parser.Value(func(attributeOrMethod *AttributeOrMethod) parser.Parse {
+	return parser.Switch(
+		parser.Case(parser.Token(Symbol, OpenBracket),
+			parser.Store(&attributeOrMethod.Method, parser.Func(parseMethod))),
+		parser.Case(parser.Token(Symbol, Equals),
+			parser.Store(&attributeOrMethod.Attribute, parser.Func(parseAttribute))))
+})
 
 func parseAttribute(p *parser.Parser) (*Attribute, error) {
 	attribute := &Attribute{}
