6f868c81f192caea1785f9345a69354618445507
Zuma <zuma@zuma.eu.org>
2026-09-26 17:46:58 +0100

    Refactored call parse

--- language/calls.go
+++ language/calls.go
@@ -1,6 +1,8 @@
 package language
 
 import (
+	"errors"
+
 	"github.com/zuma206/sb3c/lexer"
 	"github.com/zuma206/sb3c/parser"
 	"github.com/zuma206/sb3c/utils"
@@ -11,24 +13,34 @@ type Call struct {
 	Args *utils.List[*Expression]
 }
 
-var call = parser.Value(func(call *Call) parser.ParseAny {
-	return parser.All(
-		parser.Store(&call.Path,
-			parser.OneOf(parser.Type(Identifier), parser.Type(Path)),
-		),
-		parser.Optional(parser.Type(Whitespace)),
-		parser.Token(Symbol, OpenBracket),
-		parser.Store(&call.Args, parser.Until(
-			parser.Affix(
-				parser.Optional(parser.Type(Whitespace)),
-				expression,
-				parser.All(
-					parser.Optional(parser.Type(Whitespace)),
-					parser.Token(Symbol, Comma),
-				),
+var FailedCallParseErr = errors.New("failed call parse")
+
+var call = parser.Err(FailedCallParseErr,
+	parser.Value(func(call *Call) parser.ParseAny {
+		return parser.All(
+			parser.Store(&call.Path,
+				parser.OneOf(parser.Type(Identifier), parser.Type(Path)),
 			),
-			parser.Token(Symbol, CloseBracket),
-			parser.ConsumeCondition,
-		)),
-	)
-})
+			parser.Optional(parser.Type(Whitespace)),
+			parser.Token(Symbol, OpenBracket),
+			parser.Store(&call.Args, callArgs),
+		)
+	}),
+)
+
+var FailedCallArgsParseErr = errors.New("failed call args parse")
+
+var callArgs = parser.Err(FailedCallArgsParseErr,
+	parser.Until(
+		parser.Affix(
+			parser.Optional(parser.Type(Whitespace)),
+			expression,
+			parser.All(
+				parser.Optional(parser.Type(Whitespace)),
+				parser.Token(Symbol, Comma),
+			),
+		),
+		parser.Token(Symbol, CloseBracket),
+		parser.ConsumeCondition,
+	),
+)
